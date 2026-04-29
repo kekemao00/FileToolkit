b291d51899ad647bebd23fa56783ea9844bac9b1
kekemao00 <kekemao00@gmail.com>
2026-04-29 21:37:54 +0800

    ci: 修复 macOS 打包脚本空参数

--- file-toolkit/build/build_macos.sh
+++ file-toolkit/build/build_macos.sh
@@ -8,24 +8,24 @@ if [ "$arch" = "x64" ]; then
   arch="x86_64"
 fi
 
-arch_args=()
+set -- build macos
+
 if [ "$arch" != "universal" ]; then
-  arch_args=(--arch "$arch")
+  set -- "$@" --arch "$arch"
 fi
 
-icon_args=()
-if [ -f "assets/icons/app_icon.png" ]; then
-  icon_args=(--icon assets/icons/app_icon.png)
-elif [ -f "assets/icons/app_icon.icns" ]; then
-  icon_args=(--icon assets/icons/app_icon.icns)
-fi
-
-flet build macos \
-  "${arch_args[@]}" \
+set -- "$@" \
   --product "File Toolkit" \
   --artifact "File Toolkit" \
   --project "file_toolkit" \
   --bundle-id "com.kekemao00.filetoolkit" \
   --company "FileToolkit" \
-  --copyright "MIT License" \
-  "${icon_args[@]}"
+  --copyright "MIT License"
+
+if [ -f "assets/icons/app_icon.png" ]; then
+  set -- "$@" --icon assets/icons/app_icon.png
+elif [ -f "assets/icons/app_icon.icns" ]; then
+  set -- "$@" --icon assets/icons/app_icon.icns
+fi
+
+flet "$@"
